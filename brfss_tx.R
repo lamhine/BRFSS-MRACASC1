135c57d222898946bdb9bc62114bd0e4f4aff254
Tracy Lam-Hine <lamhine@gmail.com>
2023-05-15 10:42:52 -0700

    Update brfss_tx.R

--- brfss_tx.R
+++ brfss_tx.R
@@ -3,14 +3,25 @@ library(tidyverse)
 # load data 
 state_15_working_pudf <- read_dta("Library/CloudStorage/GoogleDrive-lamhine@stanford.edu/My Drive/My papers, presentations, and abstracts/BRFSS MRACASC1/Texas/PUDF15/state_15_working_pudf.dta")
 
+## select variables ##
 # detailed race variable needs to be constructed from vars starting with c07q04
-# select all vars starting with c07q04
+# ACEs questions start with TX10Q
 tx_2015 <- state_15_working_pudf %>% 
-  dplyr::select(SEQNO, starts_with("c07q04"))
+  dplyr::select(
+    SEQNO,
+    starts_with("c07q04"),
+    starts_with("TX10Q")
+    )
 
-# get counts of non-NAs by each column 
+# get counts of non-NA race responses by each of the c07q04 columns 
 # last race selected for each individual is followed by 88
-colSums(!is.na(tx_2015))
+tx_2015 %>% 
+  dplyr::select(
+    starts_with("c07q04")
+    ) %>% 
+  is.na(.) %>% 
+  `!` %>% 
+  colSums()
 
 # one person selected five races; look closer at this one individual
 tx_2015 %>% filter(!is.na(c07q04f))
